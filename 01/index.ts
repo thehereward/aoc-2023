3f0b9d49bf2f01dab1f6ca31e34f28633b6b5a12
Hereward Mills <herewardnjmills@gmail.com>
2023-12-01 05:17:35 +0000

    1b

--- 01/index.ts
+++ 01/index.ts
@@ -4,6 +4,17 @@ const logTime = getTimeLogger();
 
 var data = readFile("input");
 
+data = data
+  .map((d) => d.replace(/one/g, "one1one"))
+  .map((d) => d.replace(/two/g, "two2two"))
+  .map((d) => d.replace(/three/g, "three3three"))
+  .map((d) => d.replace(/four/g, "four4four"))
+  .map((d) => d.replace(/five/g, "five5five"))
+  .map((d) => d.replace(/six/g, "six6six"))
+  .map((d) => d.replace(/seven/g, "seven7seven"))
+  .map((d) => d.replace(/eight/g, "eight8eight"))
+  .map((d) => d.replace(/nine/g, "nine9nine"));
+
 const lines = data.map((line) => line.split("").filter((c) => parseInt(c)));
 
 const values = lines.map((line) => {
