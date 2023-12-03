aa2193941f3e2179b8364ea8b822703233014ea9
Hereward Mills <herewardnjmills@gmail.com>
2023-12-03 17:18:49 +0000

    3a

--- 03/index.ts
+++ 03/index.ts
@@ -4,16 +4,29 @@ const logTime = getTimeLogger();
 
 var data = readFile("input");
 
-const numbers: any[] = [];
+interface Number {
+  y: number;
+  x: number;
+  number: number;
+  length: number;
+  symbols: any[];
+  isEnginePart: boolean;
+  hasStar: boolean;
+}
+
+const numbers: Number[] = [];
 
 data.forEach((line, y) => {
   const matches = line.matchAll(/[0-9]+/g);
   for (const match of matches) {
     numbers.push({
       y,
-      x: match.index,
+      x: match.index || 0,
       number: parseInt(match[0]),
       length: match[0].length,
+      symbols: [],
+      isEnginePart: false,
+      hasStar: false,
     });
   }
 });
@@ -24,6 +37,8 @@ const xMax = data[0].length - 1;
 const allSymbols = data.map((line) => line.split(""));
 
 // console.log({ allSymbols });
+
+const stars: any[] = [];
 
 numbers.forEach((number) => {
   const y = number.y;
@@ -49,14 +64,25 @@ numbers.forEach((number) => {
       return;
     }
 
-    if (/[0-9\.]/.test(allSymbols[y][x])) {
+    const symbol = allSymbols[y][x];
+    if (/[0-9\.]/.test(symbol)) {
       return;
     }
     // console.log(allSymbols[y][x]);
     // Is an engine part
     number.isEnginePart = true;
+    if (number.hasStar) {
+      return;
+    }
+    if (symbol == "*") {
+      number.hasStar = symbol == "*";
+      number.symbols.push([y, x, symbol]);
+      stars.push([y, x, number.number]);
+    }
   });
 });
+
+console.log(stars);
 
 const sum = numbers.reduce((a, c) => {
   if (c.isEnginePart) {
@@ -66,15 +92,27 @@ const sum = numbers.reduce((a, c) => {
   return a;
 }, 0);
 
-console.log({ sum });
+const starSet: Record<string, number[]> = {};
 
-// for (var y = 0; y < data.length; y = y + 1) {
-//   for (var x = 0; x < data[0].length; x = x + 1) {
-//     console.log(data[y][x]);
-//   }
-// }
+stars.forEach((star) => {
+  const [y, x, number] = star;
+  const key = `${y}|${x}`;
+  if (!starSet[key]) {
+    starSet[key] = [];
+  }
+  starSet[key].push(number);
+});
 
-// console.log({ data });
+const keys = Object.keys(starSet);
+const ratios: number[] = [];
+keys.forEach((key) => {
+  const numbers = starSet[key];
+  if (numbers.length == 2) {
+    ratios.push(numbers[0] * numbers[1]);
+  }
+});
+const answer = ratios.reduce((a, c) => a + c);
+console.log(answer);
 
 logTime("Part 1");
 
