5577d1e7c60f068e18d28a8528b938c6c4708e49
Hereward Mills <herewardnjmills@gmail.com>
2023-12-03 05:27:44 +0000

    3a

--- 03/index.ts
+++ 03/index.ts
@@ -4,7 +4,77 @@ const logTime = getTimeLogger();
 
 var data = readFile("input");
 
-console.log({ data });
+const numbers: any[] = [];
+
+data.forEach((line, y) => {
+  const matches = line.matchAll(/[0-9]+/g);
+  for (const match of matches) {
+    numbers.push({
+      y,
+      x: match.index,
+      number: parseInt(match[0]),
+      length: match[0].length,
+    });
+  }
+});
+
+const yMax = data.length - 1;
+const xMax = data[0].length - 1;
+
+const allSymbols = data.map((line) => line.split(""));
+
+// console.log({ allSymbols });
+
+numbers.forEach((number) => {
+  const y = number.y;
+  const coreCells: any[] = [];
+  const indexes = [...Array(number.length).keys()].map((i) => i + number.x);
+  const allCells = indexes.flatMap((i) => {
+    return [
+      [y, i],
+      [y + 1, i],
+      [y - 1, i],
+      [y, i + 1],
+      [y + 1, i + 1],
+      [y - 1, i + 1],
+      [y, i - 1],
+      [y + 1, i - 1],
+      [y - 1, i - 1],
+    ];
+  });
+
+  allCells.forEach((cell) => {
+    const [y, x] = cell;
+    if (y < 0 || x < 0 || y > yMax || x > xMax) {
+      return;
+    }
+
+    if (/[0-9\.]/.test(allSymbols[y][x])) {
+      return;
+    }
+    // console.log(allSymbols[y][x]);
+    // Is an engine part
+    number.isEnginePart = true;
+  });
+});
+
+const sum = numbers.reduce((a, c) => {
+  if (c.isEnginePart) {
+    return a + c.number;
+  }
+
+  return a;
+}, 0);
+
+console.log({ sum });
+
+// for (var y = 0; y < data.length; y = y + 1) {
+//   for (var x = 0; x < data[0].length; x = x + 1) {
+//     console.log(data[y][x]);
+//   }
+// }
+
+// console.log({ data });
 
 logTime("Part 1");
 
