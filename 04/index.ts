c049916ea99181143c49242341aac07e6a309970
Hereward Mills <herewardnjmills@gmail.com>
2023-12-04 05:34:38 +0000

    4b

--- 04/index.ts
+++ 04/index.ts
@@ -7,10 +7,54 @@ const logTime = getTimeLogger();
 
 var data = readFile("input");
 
+function getNumbers(line: string) {
+  return line
+    .trim()
+    .split(" ")
+    .map((s) => parseInt(s));
+}
+
+const cards = data.map((line, i) => {
+  const indexOfColon = line.indexOf(":");
+  const numbers = line.slice(indexOfColon + 1);
+  const [winningString, otherString] = numbers.split("|");
+  return {
+    cardNumber: i + 1,
+    winningNumbers: getNumbers(winningString),
+    otherNumbers: getNumbers(otherString),
+    score: 0,
+    numberOfCopies: 1,
+  };
+});
+
+cards.map((card, i) => {
+  let cardsToCopy = 0;
+  card.otherNumbers.forEach((number) => {
+    const numberOfMatches = card.winningNumbers.indexOf(number);
+    if (numberOfMatches != -1) {
+      cardsToCopy = cardsToCopy + 1;
+      if (card.score == 0) {
+        card.score = 1;
+      } else {
+        card.score = card.score * 2;
+      }
+    }
+  });
+  if (cardsToCopy > 0) {
+    for (var j = i + 1; j <= i + cardsToCopy; j = j + 1) {
+      cards[j].numberOfCopies = cards[j].numberOfCopies + card.numberOfCopies;
+    }
+  }
+});
+
 logTime("Part 1");
+
+const score = cards.reduce((a, c) => a + c.score, 0);
+console.log(score);
 
 logTime("Part 2");
 
-console.log({ data });
+const numberOfCopies = cards.reduce((a, c) => a + c.numberOfCopies, 0);
+console.log(numberOfCopies);
 
 export {};
