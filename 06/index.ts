780b1b7b83c08403a0d9747a1f96fa03619d816f
Hereward Mills <herewardnjmills@gmail.com>
2023-12-06 05:09:03 +0000

    6a

--- 06/index.ts
+++ 06/index.ts
@@ -7,7 +7,41 @@ const logTime = getTimeLogger();
 
 var data = readFile("input");
 
-console.log({ data });
+const times = data[0]
+  .split(":")[1]
+  .trim()
+  .split(" ")
+  .map((c) => parseInt(c))
+  .filter((n) => !Number.isNaN(n));
+
+const distances = data[1]
+  .split(":")[1]
+  .trim()
+  .split(" ")
+  .map((c) => parseInt(c))
+  .filter((n) => !Number.isNaN(n));
+
+const races: number[][] = [];
+for (var i = 0; i < times.length; i = i + 1) {
+  races.push([times[i], distances[i]]);
+}
+
+const waysToWinArray: number[] = [];
+races.forEach((race) => {
+  let waysToWin = 0;
+  const [time, recordDistance] = race;
+  for (var speed = 0; speed < time; speed = speed + 1) {
+    const distance = speed * (time - speed);
+    if (distance > recordDistance) {
+      waysToWin = waysToWin + 1;
+    }
+  }
+  waysToWinArray.push(waysToWin);
+});
+
+console.log({ waysToWinArray });
+
+console.log(waysToWinArray.reduce((a, c) => a * c));
 
 logTime("Part 1");
 
