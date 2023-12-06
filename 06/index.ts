253f42711b9a63ce3264fad4ad5faf9d6bdb5f13
Hereward Mills <herewardnjmills@gmail.com>
2023-12-06 08:23:27 +0000

    6 - tidied

--- 06/index.ts
+++ 06/index.ts
@@ -26,38 +26,37 @@ for (var i = 0; i < times.length; i = i + 1) {
   races.push([times[i], distances[i]]);
 }
 
-const waysToWinArray: number[] = [];
-races.forEach((race) => {
-  let waysToWin = 0;
-  const [time, recordDistance] = race;
-  for (var speed = 0; speed < time; speed = speed + 1) {
-    const distance = speed * (time - speed);
-    if (distance > recordDistance) {
-      waysToWin = waysToWin + 1;
-    }
-  }
-  waysToWinArray.push(waysToWin);
+const waysToWinArray = races.map((race) => {
+  return countWaysToWin(race[0], race[1]);
 });
 
-console.log({ waysToWinArray });
-
 console.log(waysToWinArray.reduce((a, c) => a * c));
-
 logTime("Part 1");
 
-const time = parseInt(data[0].split(":")[1].trim().replace(/ /g, ""));
-const maxDistance = parseInt(data[1].split(":")[1].trim().replace(/ /g, ""));
-console.log({ time, maxDistance });
+const time = parseInt(times.join(""));
+const maxDistance = parseInt(distances.join(""));
 
-let waysToWin = 0;
-for (var speed = 0; speed < time; speed = speed + 1) {
-  const distance = speed * (time - speed);
-  if (distance > maxDistance) {
-    waysToWin = waysToWin + 1;
-  }
-}
+const waysToWin = countWaysToWin(time, maxDistance);
 console.log(waysToWin);
 
 logTime("Part 2");
 
 export {};
+
+function countWaysToWin(time: number, recordDistance: number) {
+  let waysToWin = 0;
+  let staredWinning = false;
+  for (var speed = 0; speed < time; speed = speed + 1) {
+    const distance = speed * (time - speed);
+    if (distance > recordDistance) {
+      waysToWin = waysToWin + 1;
+      staredWinning = true;
+    } else {
+      if (staredWinning) {
+        // Once you've stopped winning you can stop checking
+        break;
+      }
+    }
+  }
+  return waysToWin;
+}
