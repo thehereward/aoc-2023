005ae4bed516534e308249c3868fc66bb9e82db3
Hereward Mills <herewardnjmills@gmail.com>
2023-12-06 05:12:55 +0000

    6b

--- 06/index.ts
+++ 06/index.ts
@@ -45,6 +45,19 @@ console.log(waysToWinArray.reduce((a, c) => a * c));
 
 logTime("Part 1");
 
+const time = parseInt(data[0].split(":")[1].trim().replace(/ /g, ""));
+const maxDistance = parseInt(data[1].split(":")[1].trim().replace(/ /g, ""));
+console.log({ time, maxDistance });
+
+let waysToWin = 0;
+for (var speed = 0; speed < time; speed = speed + 1) {
+  const distance = speed * (time - speed);
+  if (distance > maxDistance) {
+    waysToWin = waysToWin + 1;
+  }
+}
+console.log(waysToWin);
+
 logTime("Part 2");
 
 export {};
