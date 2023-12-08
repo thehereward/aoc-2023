b7fea93ab4d8e9bff3b3ba5615e97233791ccdf7
Hereward Mills <herewardnjmills@gmail.com>
2023-12-08 05:13:19 +0000

    8a

--- 08/index.ts
+++ 08/index.ts
@@ -6,7 +6,37 @@ import { get0To } from "../common/range";
 const logTime = getTimeLogger();
 
 var data = readFile("input");
-console.log({ data });
+
+const LRSequence = data[0].split("").map((c) => (c == "L" ? 0 : 1));
+
+console.log({ LRSequence });
+
+data = data.slice(2);
+const regex = /^([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)$/;
+const instructions: Record<string, string[]> = {};
+
+data.forEach((line) => {
+  const match = line.match(regex);
+  if (match) {
+    const [_, key, L, R] = match;
+    instructions[key] = [L, R];
+  }
+});
+
+let location = "AAA";
+let count = 0;
+
+while (location != "ZZZ") {
+  const direction = LRSequence[count % LRSequence.length];
+  //   console.log({ location, count, direction });
+  location = instructions[location][direction];
+  count = count + 1;
+  //   if (count > 10) {
+  //     break;
+  //   }
+}
+
+console.log({ count });
 
 logTime("Part 1");
 
