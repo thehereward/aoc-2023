89bcd536d13e46016f203d780a59a933c3ab2054
Hereward Mills <herewardnjmills@gmail.com>
2023-12-09 05:18:30 +0000

    9a

--- 09/index.ts
+++ 09/index.ts
@@ -6,8 +6,40 @@ import { get0To } from "../common/range";
 const logTime = getTimeLogger();
 
 var data = readFile("input");
-console.log({ data });
+const lines = data.map((line) => line.split(" ").map((c) => parseInt(c)));
+// console.log({ lines });
 
+const sequences = lines.map((line) => [line]);
+
+const answers: number[] = [];
+sequences.forEach((sequence) => {
+  const initialSequence = sequence[0];
+  //   console.log(initialSequence);
+  let nextSequence = initialSequence;
+  let workingSequence = initialSequence;
+  while (!nextSequence.every((char) => char == 0)) {
+    nextSequence = [];
+    for (var i = 1; i < workingSequence.length; i++) {
+      const next = workingSequence[i] - workingSequence[i - 1];
+      nextSequence.push(next);
+    }
+    workingSequence = nextSequence;
+    // console.log(nextSequence);
+    sequence.push(nextSequence);
+  }
+
+  //   console.log(sequence);
+  let c = 0;
+  for (var i = sequence.length - 2; i >= 0; i--) {
+    const s = sequence[i];
+    const char = s[s.length - 1];
+    c = char + c;
+  }
+  answers.push(c);
+});
+console.log(answers);
+
+console.log(answers.reduce(sum));
 logTime("Part 1");
 
 logTime("Part 2");
