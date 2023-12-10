a22ea397fde8144afa08cddea3b0ecea5a423301
Hereward Mills <herewardnjmills@gmail.com>
2023-12-10 07:41:43 +0000

    10b but still no

--- 10/index.ts
+++ 10/index.ts
@@ -76,15 +76,17 @@ const cellsInsidebyX: Record<string, boolean> = {};
 const cellsInsidebyY: string[] = [];
 
 const allCharsX: string[][] = [];
+const allSegmentsX: number[][] = [];
 for (var y = 0; y <= yMax; y++) {
   var lastXWasLoop = false;
   var inLoopX = false;
   const chars: string[] = [];
+  const segmentsToWest: number[] = [];
+  var previousSegments = 0;
 
   for (var x = 0; x <= xMax; x++) {
     const key = toKey(x, y);
     const char = grid[key];
-    // const charNext = grid[toKey(x + 1, y)];
     var enteringLoopX = false;
     const currentCharIsLoop = isLoop(char);
 
@@ -99,28 +101,74 @@ for (var y = 0; y <= yMax; y++) {
     }
 
     chars.push(inLoopX ? "I" : char);
-    if (inLoopX) {
-      cellsInsidebyX[key] = true;
-    }
 
     if (enteringLoopX) {
       inLoopX = true;
     }
+    if (isLoop(char) && !connectsEast(char)) {
+      previousSegments++;
+    }
+    segmentsToWest.push(isLoop(char) ? NaN : previousSegments);
     lastXWasLoop = isLoop(char);
   }
   allCharsX.push(chars);
+  allSegmentsX.push(segmentsToWest);
 }
 
+const allCharsEast: string[][] = [];
+const allSegmentsEast: number[][] = [];
+for (var y = 0; y <= yMax; y++) {
+  var lastXWasLoop = false;
+  var inLoopX = false;
+  const chars: string[] = [];
+  const segmentsToEast: number[] = [];
+  var previousSegments = 0;
+
+  for (var x = xMax; x >= 0; x--) {
+    const key = toKey(x, y);
+    const char = grid[key];
+    var enteringLoopX = false;
+    const currentCharIsLoop = isLoop(char);
+
+    if (!inLoopX) {
+      if (currentCharIsLoop) {
+        enteringLoopX = true;
+      }
+    } else {
+      if (currentCharIsLoop) {
+        inLoopX = false;
+      }
+    }
+
+    chars.push(inLoopX ? "I" : char);
+
+    if (enteringLoopX) {
+      inLoopX = true;
+    }
+    if (isLoop(char) && !connectsWest(char)) {
+      previousSegments++;
+    }
+    segmentsToEast.push(isLoop(char) ? NaN : previousSegments);
+    lastXWasLoop = isLoop(char);
+  }
+  allCharsEast.push(chars.reverse());
+  allSegmentsEast.push(segmentsToEast.reverse());
+}
+
+// allCharsX.forEach((chars) => console.log(chars.join("")));
+
 const allCharsY: string[][] = [];
+const allSegmentsY: number[][] = [];
 for (var x = 0; x <= xMax; x++) {
   var lastXWasLoop = false;
   var inLoopX = false;
   const chars: string[] = [];
+  const segmentsToWest: number[] = [];
+  var previousSegments = 0;
 
   for (var y = 0; y <= yMax; y++) {
     const key = toKey(x, y);
     const char = grid[key];
-    // const charNext = grid[toKey(x + 1, y)];
     var enteringLoopX = false;
     const currentCharIsLoop = isLoop(char);
 
@@ -136,19 +184,168 @@ for (var x = 0; x <= xMax; x++) {
 
     chars.push(inLoopX ? "I" : char);
 
-    if (inLoopX && cellsInsidebyX[key]) {
-      cellsInsidebyY.push(key);
+    if (enteringLoopX) {
+      inLoopX = true;
     }
+    if (isLoop(char) && !connectsSouth(char)) {
+      previousSegments++;
+    }
+    segmentsToWest.push(isLoop(char) ? NaN : previousSegments);
+    lastXWasLoop = isLoop(char);
+  }
+  allCharsY.push(chars);
+  allSegmentsY.push(segmentsToWest);
+}
+const mapped = allSegmentsY[0].map((_, colIndex) =>
+  allSegmentsY.map((row) => row[colIndex])
+);
+
+const allCharsSouth: string[][] = [];
+const allSegmentsSouth: number[][] = [];
+for (var x = 0; x <= xMax; x++) {
+  var lastXWasLoop = false;
+  var inLoopX = false;
+  const chars: string[] = [];
+  const segmentsToWest: number[] = [];
+  var previousSegments = 0;
+
+  for (var y = yMax; y >= 0; y--) {
+    const key = toKey(x, y);
+    const char = grid[key];
+    var enteringLoopX = false;
+    const currentCharIsLoop = isLoop(char);
+
+    if (!inLoopX) {
+      if (currentCharIsLoop) {
+        enteringLoopX = true;
+      }
+    } else {
+      if (currentCharIsLoop) {
+        inLoopX = false;
+      }
+    }
+
+    chars.push(inLoopX ? "I" : char);
 
     if (enteringLoopX) {
       inLoopX = true;
     }
+    if (isLoop(char) && !connectsSouth(char)) {
+      previousSegments++;
+    }
+    segmentsToWest.push(isLoop(char) ? NaN : previousSegments);
     lastXWasLoop = isLoop(char);
   }
-  allCharsY.push(chars);
+  allCharsSouth.push(chars.reverse());
+  allSegmentsSouth.push(segmentsToWest.reverse());
+}
+const mappedSouth = allSegmentsSouth[0].map((_, colIndex) =>
+  allSegmentsSouth.map((row) => row[colIndex])
+);
+
+const output: number[][] = [];
+for (var y = 0; y <= yMax; y++) {
+  output.push([]);
+  for (var x = 0; x <= xMax; x++) {
+    const w = allSegmentsX[y][x];
+    const n = mapped[y][x];
+    const e = allSegmentsEast[y][x];
+    const s = mappedSouth[y][x];
+
+    var char = 0;
+    if (w == 0 || n == 0 || e == 0 || s == 0) {
+      setTo0(x, y);
+      char = 0;
+    } else if (isEven(w) && isEven(e)) {
+      setTo0(x, y);
+      char = 0;
+    } else if (isEven(n) && isEven(s)) {
+      setTo0(x, y);
+      char = 0;
+    } else {
+      char = 1;
+    }
+    //  else if (a % 2 == 0 && b % 2 == 0 && c % 2 == 0 && d % 2 == 0) {
+    //   allSegmentsX[y][x] = 0;
+    //   mapped[y][x] = 0;
+    //   allSegmentsEast[y][x] = 0;
+    //   mappedSouth[y][x] = 0;
+
+    //   char = 0;
+    // }
+    output[y].push(char);
+  }
 }
 
-console.log(cellsInsidebyY.length);
+function isEven(x: number) {
+  return x % 2 == 0;
+}
+
+function setTo0(x: number, y: number) {
+  allSegmentsX[y][x] = 0;
+  mapped[y][x] = 0;
+  allSegmentsEast[y][x] = 0;
+  mappedSouth[y][x] = 0;
+}
+
+function print(num: number): string {
+  if (isNaN(num)) {
+    return ".";
+  } else {
+    return num.toString();
+  }
+}
+
+allSegmentsX.forEach((chars) => console.log(chars.map(print).join("")));
+console.log("");
+mapped.forEach((chars) => console.log(chars.map(print).join("")));
+console.log("");
+allSegmentsEast.forEach((chars) => console.log(chars.map(print).join("")));
+console.log("");
+mappedSouth.forEach((chars) => console.log(chars.map(print).join("")));
+
+const oooo = mappedSouth
+  .flatMap((line) => line)
+  .filter((x) => !isNaN(x) && x > 0);
+console.log(oooo.length);
+
+// for (var x = 0; x <= xMax; x++) {
+//   var lastXWasLoop = false;
+//   var inLoopX = false;
+//   const chars: string[] = [];
+
+//   for (var y = 0; y <= yMax; y++) {
+//     const key = toKey(x, y);
+//     const char = grid[key];
+//     // const charNext = grid[toKey(x + 1, y)];
+//     var enteringLoopX = false;
+//     const currentCharIsLoop = isLoop(char);
+
+//     if (!inLoopX) {
+//       if (currentCharIsLoop) {
+//         enteringLoopX = true;
+//       }
+//     } else {
+//       if (currentCharIsLoop) {
+//         inLoopX = false;
+//       }
+//     }
+
+//     chars.push(inLoopX ? "I" : char);
+
+//     if (inLoopX && cellsInsidebyX[key]) {
+//       cellsInsidebyY.push(key);
+//     }
+
+//     if (enteringLoopX) {
+//       inLoopX = true;
+//     }
+//     lastXWasLoop = isLoop(char);
+//   }
+//   allCharsY.push(chars);
+// }
+
+// console.log(cellsInsidebyY.length);
 
 logTime("Part 2");
 
