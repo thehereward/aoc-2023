d87638e8995bed9ac3e5c4dad6fc5bf3b2072292
Hereward Mills <herewardnjmills@gmail.com>
2023-12-12 21:33:47 +0000

    aaaaargh

--- 12/index.ts
+++ 12/index.ts
@@ -13,7 +13,7 @@ var records = lines.map((line) => {
   const c = new Array(repetitions);
   const b = c.fill(line[1]).join(",");
   const d = new Array(repetitions);
-  const e = d.fill(line[0]).join("?")
+  const e = d.fill(line[0]).join("?");
 
   return {
     record: e,
@@ -24,142 +24,78 @@ var records = lines.map((line) => {
 });
 logTime("Records made");
 
-const toProcess = records
-  .map((r) => {
-    return { check: r.check, record: r.record };
-  })
-  // .filter((x, i) => i == 5);
+const toProcess = records.map((r) => {
+  return { check: r.check, record: r.record };
+});
 
-
-// console.log(toProcess)
-// console.log("")
-var record = toProcess.shift();
-
-var count = 0;
-var iterations = 0
-while (!!record) {
-  if (iterations % 1000000 == 0){
-    logTime(`Got to ${iterations}`)
-  }
-  const validNewStrings: any[] = processRecord(record);
-
-  toProcess.unshift(...validNewStrings)
-  // console.log(toProcess);
-  // console.log({ count })
-  record = toProcess.shift()
-  // console.log("")
-  iterations++
-}
-
-// console.log(655360 - 506250)
-// ??????????
-// ?????????
-console.log({ count });
-
-const regex = /^\.*#{1}\.+#{1}\.+#{3}\.*$/;
+const memory = new Map<string, number>();
+var misses = 0;
+var hits = 0;
+const p = toProcess.map((record) => processRecord(record)).reduce(sum);
+// 2771633347978 too high
+console.log({ p, hits, misses });
 
 logTime("Part 1");
 
 logTime("Part 2");
 
-export { };
-function processRecord(record: { record: string; check: number[] }) {
-  // console.log("");
-  // console.log({ record });
-  const blocks = record.check.map(c => c)
-  var r = record.record;
+export {};
 
-  if (noHashRemaining(r) && blocks.length == 0) {
-    // finished
-    count++
-    return []
+function processRecord(record: { record: string; check: number[] }): number {
+  const blocks1 = record.check;
+  const r1 = record.record;
+  const key = `${r1}${blocks1}`;
+  if (!memory.has(key)) {
+    misses++;
+    if (blocks1.length == 0) {
+      return noHashRemaining(r1) ? 1 : 0;
+    }
+
+    if (r1.length == 0) {
+      return 0;
+    }
+
+    if (r1[0] == ".") {
+      return processRecord({
+        record: r1.slice(1),
+        check: blocks1,
+      });
+    }
+
+    const [head, ...rest] = blocks1;
+
+    if (head > r1.length) {
+      return 0;
+    }
+
+    const match =
+      r1
+        .slice(0, head)
+        .split("")
+        .every((char) => char == "?" || char == "#") && r1[head] != "#";
+
+    const sibCount =
+      r1[0] === "#"
+        ? 0
+        : processRecord({ record: r1.slice(1), check: blocks1 });
+
+    const childCount = match
+      ? processRecord({
+          record: r1.slice(head + 1),
+          check: rest,
+        })
+      : 0;
+
+    const answer = sibCount + childCount;
+
+    memory.set(key, answer);
+  } else {
+    hits++;
   }
 
-  if (noHashRemaining(r) && noQuestionRemaining(r) && blocks.length != 0) {
-    // error
-    return []
-  }
-
-  if (blocks.length == 0) {
-    return []
-  }
-
-  const block = blocks.shift()
-  if (block == undefined) {
-    // should not happen
-    return []
-  }
-  const regexToUse = new RegExp(`[\#\?]{${block}}(?![\#])`);
-  var match44 = r.match(regexToUse)
-  // console.log({match44})
-  if (!match44) {
-    return []
-  }
-  const { index } = match44
-  if (index == undefined) {
-    // should never happen
-    return []
-  }
-
-  const before = r.slice(0, index)
-  if (!noHashRemaining(before)){
-    return []
-  }
-
-  const newRecords: { record: string; check: number[] }[] = []
-  if (match44[0][0] != '#') {
-    newRecords.push({
-      check: [block, ...blocks],
-      record: r.slice(index + 1)
-    })
-  }
-  newRecords.push({
-    check: blocks,
-    record: r.slice(index + block + 1)
-  })
-  return newRecords
-
-
-  // console.log({ match44, blocks })
-  // if (index != undefined) {
-  //   const nextChar = r[index + block]
-  //   // console.log({ match44, blocks, nextChar })
-  //   if (nextChar != '#') {
-  //     const after = r.slice(index + block + 1)
-  //     newRecords.push(
-  //       {
-  //         check: blocks,
-  //         record: after
-  //       }
-  //     )
-
-  //     r = r.slice(index + 1)
-  //   } else {
-  //     r = r.slice(1)
-  //   }
-  // }
-  // return newRecords
+  return memory.get(key) || 0;
 }
 
 function noHashRemaining(r: string) {
   return r.indexOf("#") == -1;
-}
-
-function noQuestionRemaining(r: string) {
-  return r.indexOf("?") == -1;
-}
-
-function isInvalid(t: { record: string; check: number[] }) {
-  const invalid_blocks_left =
-    t.check.length > 0 && t.record.replace(".", "").length == 0;
-  const invalid_hashes_left =
-    t.check.length == 0 && t.record.replace("#", "").length != 0;
-  const invalid = invalid_blocks_left || invalid_hashes_left;
-  return invalid;
-}
-
-function isFinished(t: { record: string; check: number[] }) {
-  const finished =
-    t.check.length == 0 && t.record.replace("#", "").length == t.record.length;
-  return finished;
 }
