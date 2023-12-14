e32e9250fe5bc1262fe7205b8e071dece1beeb7a
Hereward Mills <herewardnjmills@gmail.com>
2023-12-14 05:52:14 +0000

    wip

--- 14/index.ts
+++ 14/index.ts
@@ -24,39 +24,116 @@ const xMax = lines[0].length;
 
 // console.log("");
 
-for (var y = 0; y < yMax; y++) {
-  for (var x = 0; x < xMax; x++) {
-    const char = lines[y][x];
-    if (char == "O") {
-      //   console.log(x, y, char);
-
-      var yIndexOfObstacle = -1;
-      for (var seakNorth = y - 1; seakNorth >= 0; seakNorth--) {
-        //   console.log(y, seakNorth);
-        if (lines[seakNorth][x] != ".") {
-          yIndexOfObstacle = seakNorth;
-          break;
+function tiltNorth() {
+  for (var y = 0; y < yMax; y++) {
+    for (var x = 0; x < xMax; x++) {
+      const char = lines[y][x];
+      if (char == "O") {
+        var yIndexOfObstacle = -1;
+        for (var seakNorth = y - 1; seakNorth >= 0; seakNorth--) {
+          if (lines[seakNorth][x] != ".") {
+            yIndexOfObstacle = seakNorth;
+            break;
+          }
         }
+        lines[y][x] = ".";
+        lines[yIndexOfObstacle + 1][x] = char;
       }
-      lines[y][x] = ".";
-      lines[yIndexOfObstacle + 1][x] = char;
-      //   printlines(lines);
-      //   console.log("");
-      //   console.log(x, y, char, yIndexOfObstacle);
     }
   }
 }
+
+function tiltSouth() {
+  for (var y = yMax - 1; y >= 0; y--) {
+    for (var x = 0; x < xMax; x++) {
+      const char = lines[y][x];
+      if (char == "O") {
+        var yIndexOfObstacle = yMax;
+        for (var seakSouth = y + 1; seakSouth < yMax; seakSouth++) {
+          if (lines[seakSouth][x] != ".") {
+            yIndexOfObstacle = seakSouth;
+            break;
+          }
+        }
+        lines[y][x] = ".";
+        lines[yIndexOfObstacle - 1][x] = char;
+      }
+    }
+  }
+}
+
+function tiltWest() {
+  for (var x = 0; x < xMax; x++) {
+    for (var y = 0; y < yMax; y++) {
+      const char = lines[y][x];
+      if (char == "O") {
+        var xIndexOfObstacle = -1;
+        for (var seakWest = x - 1; seakWest >= 0; seakWest--) {
+          if (lines[y][seakWest] != ".") {
+            xIndexOfObstacle = seakWest;
+            break;
+          }
+        }
+        lines[y][x] = ".";
+        lines[y][xIndexOfObstacle + 1] = char;
+      }
+    }
+  }
+}
+
+function tiltEast() {
+  for (var x = xMax - 1; x >= 0; x--) {
+    for (var y = 0; y < yMax; y++) {
+      const char = lines[y][x];
+      if (char == "O") {
+        var xIndexOfObstacle = yMax;
+        for (var seakEast = x + 1; seakEast < xMax; seakEast++) {
+          if (lines[y][seakEast] != ".") {
+            xIndexOfObstacle = seakEast;
+            break;
+          }
+        }
+        lines[y][x] = ".";
+        lines[y][xIndexOfObstacle - 1] = char;
+      }
+    }
+  }
+}
+
+tiltNorth();
 
 const part1 = lines
   .map((line) => line.filter((char) => char == "O").length)
   .map((sum, i) => sum * (yMax - i))
   .reduce(sum);
 console.log(part1);
-// printlines(lines);
 
 // console.log({ everything });
 
 logTime("Part 1");
+
+tiltWest();
+tiltSouth();
+tiltEast();
+
+var count = 1;
+
+function getState() {
+  const state = lines.map((line) => line.join("")).join("");
+}
+
+for (count = 1; count < 1000000000; count++) {
+  tiltNorth();
+  tiltWest();
+  tiltSouth();
+  tiltEast();
+  if (count % 1000000 == 0) {
+    logTime(`Completed cycles: ${count}`);
+  }
+}
+
+logTime(`After cycles: ${count}`);
+printlines(lines);
 
 logTime("Part 2");
 
