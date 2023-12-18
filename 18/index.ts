af36c69d5e330170dd237a38d77ee1a94c06ba92
Hereward Mills <herewardnjmills@gmail.com>
2023-12-18 05:42:28 +0000

    refactor

--- 18/index.ts
+++ 18/index.ts
@@ -16,16 +16,7 @@ const instructions = lines.map((line) => {
 });
 // console.log({ instructions });
 
-var point = [-1, -1];
-
-const points = [point];
-instructions.forEach((instruction) => {
-  const { direction, length } = instruction;
-  for (var i = 0; i < length; i++) {
-    point = getNextPoint(point, direction);
-    points.push(point);
-  }
-});
+const points = getPoints(instructions);
 
 var yMin = Infinity;
 var xMin = Infinity;
@@ -57,6 +48,20 @@ grid.forEach((point) => {
   allPoints.add(toKey(x, y));
 });
 
+function getPoints(instructions: { direction: string; length: number }[]) {
+  var point = [-1, -1];
+
+  const points = [point];
+  instructions.forEach((instruction) => {
+    const { direction, length } = instruction;
+    for (var i = 0; i < length; i++) {
+      point = getNextPoint(point, direction);
+      points.push(point);
+    }
+  });
+  return points;
+}
+
 function printGrid(allPoints: Set<string>) {
   for (var y = 0; y <= yMax + 1; y++) {
     const row: string[] = [];
@@ -72,44 +77,48 @@ var fillFrom = [
   [yMax + 1, xMax + 1],
 ];
 
-// console.log({ size: (yMax + 2) * (xMax + 2) });
-
-const floodedCells: Set<string> = new Set();
-
-fillFrom.forEach((cell) => {
-  const [y, x] = cell;
-  floodedCells.add(toKey(x, y));
-});
-
-while (fillFrom.length > 0) {
-  logTime(floodedCells.size.toString());
-  fillFrom = fillFrom.flatMap((cell) => {
-    const [y, x] = cell;
-    const cells = get3By3(x, y).filter((cell) => {
-      const [y, x] = cell;
-      if (y < 0 || x < 0 || y > yMax + 1 || x > xMax + 1) {
-        return false;
-      }
-      if (floodedCells.has(toKey(x, y))) {
-        return false;
-      }
-      if (allPoints.has(toKey(x, y))) {
-        return false;
-      }
-
-      return true;
-    });
-    cells.forEach((cell) => {
-      const [y, x] = cell;
-      floodedCells.add(toKey(x, y));
-    });
-    return cells;
-  });
-}
+const floodedCells: Set<string> = fill(fillFrom, allPoints);
 
 const size = (yMax + 2) * (xMax + 2);
 const part1 = size - floodedCells.size;
 console.log({ part1 });
+
+function fill(start: number[][], allPoints: Set<string>) {
+  const floodedCells: Set<string> = new Set();
+
+  var fillFrom: number[][] = JSON.parse(JSON.stringify(start));
+  fillFrom.forEach((cell) => {
+    const [y, x] = cell;
+    floodedCells.add(toKey(x, y));
+  });
+
+  while (fillFrom.length > 0) {
+    // logTime(floodedCells.size.toString());
+    fillFrom = fillFrom.flatMap((cell) => {
+      const [y, x] = cell;
+      const cells = get3By3(x, y).filter((cell) => {
+        const [y, x] = cell;
+        if (y < 0 || x < 0 || y > yMax + 1 || x > xMax + 1) {
+          return false;
+        }
+        if (floodedCells.has(toKey(x, y))) {
+          return false;
+        }
+        if (allPoints.has(toKey(x, y))) {
+          return false;
+        }
+
+        return true;
+      });
+      cells.forEach((cell) => {
+        const [y, x] = cell;
+        floodedCells.add(toKey(x, y));
+      });
+      return cells;
+    });
+  }
+  return floodedCells;
+}
 
 function getNextPoint(point: number[], direction: string): number[] {
   const [y, x] = point;
