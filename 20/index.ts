2c153da94bc0e862208610b7ab395810928c5e45
Hereward Mills <herewardnjmills@gmail.com>
2023-12-20 05:52:03 +0000

    part 1

--- 20/index.ts
+++ 20/index.ts
@@ -5,11 +5,183 @@ import { get0To } from "../common/range";
 
 const logTime = getTimeLogger();
 
+const FlipFlopPrefix = "%";
+const ConjunctionPrefix = "&";
+
+const unfoundSet: Set<string> = new Set();
+
 var data = readFile("input");
-console.log({ data });
+type ModuleType = "FlipFlop" | "Conjunction" | "broadcaster";
+type Module = {
+  type: ModuleType;
+  name: string;
+  destinations: string[];
+  output: boolean;
+  inputs: Map<string, boolean>;
+};
+const modules: Module[] = data
+  .map((line) => line.split(" -> "))
+  .map((line) => {
+    const [source, destString] = line;
+    const destinations = destString.split(",").map((s) => s.trim());
+    var type: ModuleType;
+    var name: string = "";
+    switch (source[0]) {
+      case FlipFlopPrefix:
+        type = "FlipFlop";
+        name = source.slice(1);
+        break;
+      case ConjunctionPrefix:
+        type = "Conjunction";
+        name = source.slice(1);
+        break;
+      default:
+        type = "broadcaster";
+        name = "broadcaster";
+    }
+    return {
+      name,
+      type,
+      destinations,
+      output: false,
+      inputs: new Map(),
+    };
+  });
+
+const moduleMap: Map<string, Module> = new Map();
+modules.forEach((module) => moduleMap.set(module.name, module));
+
+// Initialise all inputs - mainly for conjunctions
+modules.forEach((module) => {
+  module.destinations.forEach((destination) => {
+    const destMod = moduleMap.get(destination);
+    if (!destMod) {
+      unfoundSet.add(destination);
+      return;
+    }
+    destMod.inputs.set(module.name, false);
+  });
+});
+
+// console.log(moduleMap);
+
+type Pulse = {
+  source: string;
+  destination: string;
+  value: boolean;
+};
+
+function sendPulse(pulse: Pulse): Pulse[] {
+  const { source, destination, value } = pulse;
+  const module = moduleMap.get(destination);
+  if (!module) {
+    unfoundSet.add(destination);
+    return [];
+  }
+  switch (module.type) {
+    case "FlipFlop":
+      if (value) {
+        return [];
+      } else {
+        module.output = !module.output;
+        return module.destinations.map((destination) => {
+          return {
+            source: module.name,
+            destination,
+            value: module.output,
+          };
+        });
+      }
+    case "Conjunction":
+      module.inputs.set(source, value);
+      var allInputsTrue = true;
+      module.inputs.forEach(
+        (value) => (allInputsTrue = allInputsTrue && value)
+      );
+      return module.destinations.map((destination) => {
+        return {
+          source: module.name,
+          destination,
+          value: !allInputsTrue,
+        };
+      });
+    case "broadcaster":
+      return module.destinations.map((destination) => {
+        return {
+          source: module.name,
+          destination,
+          value,
+        };
+      });
+  }
+}
+
+const stateAfterPressingButton: {
+  lowPulseCount: number;
+  highPulseCount: number;
+}[] = [];
+var count = 1000;
+
+while (count > 0) {
+  const { anyOutputsHigh, lowPulseCount, highPulseCount } = pressButton();
+
+  stateAfterPressingButton.push({
+    lowPulseCount,
+    highPulseCount,
+  });
+  count--;
+}
+
+// console.log(modules);
+// console.log(stateAfterPressingButton);
+const part1 = stateAfterPressingButton.reduce(
+  (a, c) => {
+    return {
+      lowPulseCount: a.lowPulseCount + c.lowPulseCount,
+      highPulseCount: a.highPulseCount + c.highPulseCount,
+    };
+  },
+  {
+    lowPulseCount: 0,
+    highPulseCount: 0,
+  }
+);
+console.log(unfoundSet);
+console.log(part1.highPulseCount * part1.lowPulseCount);
 
 logTime("Part 1");
 
 logTime("Part 2");
 
 export {};
+function pressButton() {
+  const pulses: Pulse[] = [];
+
+  pulses.push({
+    source: "button",
+    destination: "broadcaster",
+    value: false,
+  });
+
+  var highPulseCount = 0;
+  var lowPulseCount = 0;
+
+  while (pulses.length > 0) {
+    const pulse = pulses.shift();
+    if (!pulse) {
+      throw new Error();
+    }
+    if (pulse.value) {
+      highPulseCount++;
+    } else {
+      lowPulseCount++;
+    }
+    const newPulses = sendPulse(pulse);
+    pulses.push(...newPulses);
+  }
+
+  const anyOutputsHigh = modules.reduce((a, c) => {
+    return c.output || a;
+  }, false);
+  return { anyOutputsHigh, lowPulseCount, highPulseCount };
+}
