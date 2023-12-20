d7b020082296066757e9a14fe4eed1856e0f0abf
Hereward Mills <herewardnjmills@gmail.com>
2023-12-20 20:58:35 +0000

    prep

---
+++ 21/index.ts
@@ -0,0 +1,15 @@
+import { sum } from "../common";
+import { readFile, getTimeLogger } from "../common";
+import { toKey, get3By3 } from "../common/grid";
+import { get0To } from "../common/range";
+
+const logTime = getTimeLogger();
+
+var data = readFile("input");
+console.log({ data });
+
+logTime("Part 1");
+
+logTime("Part 2");
+
+export {};
